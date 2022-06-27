64b030716060a79a43eac0fe88908dddcf88dca5
omar3o3 <otantawy53@gmail.com>
2022-06-27 12:50:39 -0400

    commit tree 8e4a05b56b94cfccc6fdc40850361eb74d3e071d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,26 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params.has_key?(:name)
+      # byebug
+      search = params[:name].capitalize()
+
+      render json: Student.where(first_name: search).or(Student.where(last_name: search))
+      # if (Student.where("first_name = ?", search))
+      #   render json: Student.find_by(first_name: search)
+      # elsif (Student.where("last_name = ?", search))
+      #   render json: Student.find_by(last_name: search)
+      # end
+    else
+      render json: Student.all
+    end
+  end
+
+  def show_student_by_id
+    render json: Student.find(params[:id])
   end
 
 end
+
+# render json: Student.find_by(first_name: params[:name])
+# render json: Student.find_by(last_name: params[:name])
